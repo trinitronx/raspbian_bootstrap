52c4c8391a72587f395e4b6b8dba0c91f27850cb
Dayne Broderson <broderson@gmail.com>
2017-01-29 10:49:33 -0900

    fixing sleepy made bugs

--- dayne_raspbian_prebuilt.sh
+++ dayne_raspbian_prebuilt.sh
@@ -3,8 +3,9 @@ BUILT_CHEF_TGZ=http://n1nj4net-public.s3-website-us-west-2.amazonaws.com/raspbia
 if [ ! -f /usr/bin/chef-client ]; then
   apt-get update
   apt-get install -y curl wget git autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev ntpdate
+fi
 
-  ntpdate -u pool.ntp.org
+ntpdate -u pool.ntp.org
 
 if [ ! -d /opt/chef ] ; then
   pushd .
