66b320b613eb1c805217c656a7ebbc29ebed0c4d
Marcus Bowyer <marcus@marcusbowyer.co.uk>
2017-02-05 18:57:46 -0800

    fixing initial bugs I found while trying to get it working

--- ruby-stretch.sh
+++ ruby-stretch.sh
@@ -11,7 +11,7 @@ apt-get purge ruby -y
 DEBIAN_FRONTEND=noninteractive apt-get install -y curl ruby2.3 ruby2.3-dev autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
 
 # As we are using Stretch for the Ruby 2.3 packages not all dependencies can be correctly installed, force them
-apt-get -f install
+apt-get -f install -y
 
 gem install moneta --no-rdoc --no-ri --verbose
 gem install net-ssh-gateway --no-rdoc --no-ri --verbose
