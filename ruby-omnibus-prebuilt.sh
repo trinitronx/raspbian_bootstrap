a17cbfe0eae40b0e42af7784220da1fd6c1a3020
Dayne Broderson <broderson@gmail.com>
2019-01-15 15:23:19 -0900

    fix bash comparison logic in prebuilt script

--- ruby-omnibus-prebuilt.sh
+++ ruby-omnibus-prebuilt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "${CHEF_URL}" == "" ]; then
+if [ -z "${CHEF_URL}"  ]; then
   CHEF_URL="http://n1nj4net-public.s3-website-us-west-2.amazonaws.com/chef_14.8.10+20181204005213-1_armhf.deb"
   # not used yet
   CHEF_SHA=6a8a9fd8a5ba9ee00f1ab8eb6170fe6de8fb5d97bcd807e8366b9919140f8d2f
@@ -8,37 +8,41 @@ fi
 CHEF_DEB=$(basename "$CHEF_URL")
 
 dpkg -s chef > /dev/null 2>&1
-if [ $? == 0 ]; then
+if [ $? -eq 0 ]; then
   echo "chef package already installed"
   exit 1
 fi
 
-DEPS="curl wget git autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev ntpdate"
+DEPS="curl wget git ntpdate"
 
-dpkg -l $DEPS > /dev/null
-if [ $? != 0 ]; then
-  echo "INFO: getting system fleshed out for dev tools and ntpdate"
+dpkg -l $DEPS > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "INFO: getting system fleshed out for download utils and ntpdate"
   apt-get update
   apt-get install -y $DEPS
 else
-  echo "INFO: system setup for dev tools and ntpdate"
+  echo "INFO: system setup for download tools and ntpdate"
 fi
 
 echo "INFO: syncing time to pool.ntp.org"
 ntpdate -u pool.ntp.org
 
 cd /tmp
+
 echo "INFO: downloading pre-built chef"
 echo "INFO: $CHEF_URL"
 test -f $CHEF_DEB || curl -O "${CHEF_URL}"
-if [ $? != 0 ]; then
+if [ $? -eq 0 ]; then
+  echo "downloaded chef: $CHEF_DEB"
+else
   echo "ERROR: download of prebuilt deb failed"
   echo "ERROR: failed to download: $CHEF_URL"
   exit 1
 fi
+
 test -f $CHEF_DEB && dpkg -i $CHEF_DEB
-if [ $? != 0 ]; then
-  echo "ERROR: failed: dpkg -i $CHEF_DEB"
+if [ $? -ne 0 ]; then
+  echo "ERROR: failed install of chef: dpkg -i $CHEF_DEB"
   exit 1
 else 
   echo "INFO: install of chef successful"
